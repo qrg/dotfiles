353bbe061d9e5414db9ea300b5ef67d299e85798
qurage <qualle.rhythmus+d@gmail.com>
2012-10-11 15:30:15 +0900

    Modify sh not to make the symlink with files related to git
    * .gitignore, .gitmodules なども ~/ に symlink を作っていたので修正
    * shell script の if 文の条件式の書き方をもうちょっとなんとかしたい

--- init_ln.sh
+++ init_ln.sh
@@ -2,8 +2,18 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules' ] && [ $dotfile = '.gitignore_global' ]
+# if の条件をもっと綺麗に書きたいけど書き方がわからない
     then
+    	echo $dotfile
         ln -Fis "$PWD/$dotfile" $HOME
+
+# -F, --directory   スーパーユーザがディレクトリに対するハードリンクを
+#                    作成することを許可する。(備考: スーパーユーザであっても
+#                    システムの制限で失敗することがある)
+# -i, --interactive  対象を削除するかどうか確認メッセージを表示する
+# -s, --symbolic     ハードリンクの代わりにシンボリックリンクを作成する
+
     fi
 done
+
