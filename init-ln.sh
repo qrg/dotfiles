1c22384e896529fec9ab5f464ba30a63bc7d4829
qurage <qualle.rhythmus+d@gmail.com>
2012-10-12 05:59:50 +0900

    Update init-ln.sh
    * dotfiles/ 内のファイル名から自動的にsymlinkを作成するのをやめた
      + symlink 作成から除外するファイルを自動でやるのがわりとめんどかったので
    * いちいちシンボリックリンクを作成するコマンドを書いとくことにした

--- init-ln.sh
+++ init-ln.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+
 cd $(dirname $0)
-for dotfile in .?*
-do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules' ] && [ $dotfile = '.gitignore_global' ]
-# if の条件をもっと綺麗に書きたいけど書き方がわからない
-    then
-    	echo $dotfile
-        ln -Fis "$PWD/$dotfile" $HOME
 
-# -F, --directory   スーパーユーザがディレクトリに対するハードリンクを
-#                    作成することを許可する。(備考: スーパーユーザであっても
-#                    システムの制限で失敗することがある)
-# -i, --interactive  対象を削除するかどうか確認メッセージを表示する
-# -s, --symbolic     ハードリンクの代わりにシンボリックリンクを作成する
+ln -Fisv $PWD/.sh.d $HOME
+ln -Fisv $PWD/.vim.d $HOME
+ln -Fisv $PWD/.zsh.d $HOME
+ln -Fisv $PWD/.bashrc $HOME
+ln -Fisv $PWD/.gitignore_global $HOME
+ln -Fisv $PWD/.mayu $HOME
+ln -Fisv $PWD/.tmux.conf $HOME
+ln -Fisv $PWD/.vimrc $HOME
+ln -Fisv $PWD/.zshrc $HOME
 
-    fi
-done
+# ln
+# -d, -F, --directory    スーパーユーザがディレクトリに対するハードリンクを
+#                        作成することを許可する。(備考: スーパーユーザであっても
+#                        システムの制限で失敗することがある)
+# -i, --interactive      対象を削除するかどうか確認メッセージを表示する
+# -s, --symbolic         ハードリンクの代わりにシンボリックリンクを作成する
+# -v, --verbose          print name of each linked file
 
