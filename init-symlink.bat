b33573e6e60845d5e2bdb19c5f821192a0250234
qurage <qualle.rhythmus+d@gmail.com>
2012-10-16 08:28:00 +0900

    Change init-symlink.bat by changing from symlink to hardlink
    * Vim on Windows has a problem that replace symlink to just a file
      with writing some symlink file

--- init-symlink.bat
+++ init-symlink.bat
@@ -5,16 +5,12 @@ cd /d "%~dp0"
 mklink /D "%HOME%\.sh.d" "%HOME%\dotfiles\.sh.d"
 mklink /D "%HOME%\.vim" "%HOME%\dotfiles\.vim"
 mklink /D "%HOME%\.zsh.d" "%HOME%\dotfiles\.zsh.d"
-mklink "%HOME%\.bashrc" "%HOME%\dotfiles\.bashrc"
-mklink "%HOME%\.gitignore_global" "%HOME%\dotfiles\.gitignore_global"
-mklink "%HOME%\.mayu" "%HOME%\dotfiles\.mayu"
-mklink "%HOME%\.tmux.conf" "%HOME%\dotfiles\.tmux.conf"
-mklink "%HOME%\.vimrc" "%HOME%\dotfiles\.vimrc"
-mklink "%HOME%\.gvimrc" "%HOME%\dotfiles\.gvimrc"
-mklink "%HOME%\.zshrc" "%HOME%\dotfiles\.zshrc"
+mklink /H "%HOME%\.bashrc" "%HOME%\dotfiles\.bashrc"
+mklink /H "%HOME%\.gitignore_global" "%HOME%\dotfiles\.gitignore_global"
+mklink /H "%HOME%\.mayu" "%HOME%\dotfiles\.mayu"
+mklink /H "%HOME%\.tmux.conf" "%HOME%\dotfiles\.tmux.conf"
+mklink /H "%HOME%\.vimrc" "%HOME%\dotfiles\.vimrc"
+mklink /H "%HOME%\.gvimrc" "%HOME%\dotfiles\.gvimrc"
+mklink /H "%HOME%\.zshrc" "%HOME%\dotfiles\.zshrc"
 
 pause
-
-REM mklink /D .vim .\dotfiles\dot.vim
-REM mklink /H _vimrc .\dotfiles\dot.vimrc
-REM mklink /H _gvimrc .\dotfiles\dot.gvimrc
