8f3bd9d2c60c20222f29f158583651c7a151e2e6
qurage <qualle.rhythmus+d@gmail.com>
2013-01-25 06:13:36 +0900

    Delete unnecessary emacs packages

--- .emacs.d/packages/elpa/birds-of-paradise-plus-theme-20120811.1252/birds-of-paradise-plus-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; birds-of-paradise-plus-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "birds-of-paradise-plus-theme" "birds-of-paradise-plus-theme.el"
-;;;;;;  (20737 23823))
-;;; Generated autoloads from birds-of-paradise-plus-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("birds-of-paradise-plus-theme-pkg.el")
-;;;;;;  (20737 23823 229933))
-
-;;;***
-
-(provide 'birds-of-paradise-plus-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; birds-of-paradise-plus-theme-autoloads.el ends here
--- .emacs.d/packages/elpa/cyberpunk-theme-20121015.1228/cyberpunk-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cyberpunk-theme" "20121015.1228" "Cyberpunk Color Theme [source: github]" (quote nil))
