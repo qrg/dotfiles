25e6b140ed65388ab6784c9c3bdd03046323e3b5
qurage <qualle.rhythmus+d@gmail.com>
2013-10-22 16:37:43 +0900

    Add pyvenv path config

--- .sh.d/common.sh
+++ .sh.d/common.sh
@@ -2,8 +2,8 @@
 # path config
 # -----------------------------------------------
 CommonDir=~/dotfiles/.sh.d
-AliasesFile=${CommonDir}/aliases.sh
 EnvFile=${CommonDir}/env.sh
+AliasesFile=${CommonDir}/aliases.sh
 
 NvmFile=~/.nvm/nvm.sh
 
@@ -43,37 +43,4 @@ if [ -f ${GitCompletionFile} ]; then
 	source ${GitCompletionFile}
 fi
 
-# rbenv & phpenv -------------------------------------------------------
-# .rbenv ディレクトリが home にあればパスを通す
-# phpenv は rbenv を内部利用しているため先に rbenv に PATH を通す
-if [ -d $HOME/.rbenv/bin ]; then
-  export PATH=$PATH:$HOME/.rbenv/bin
-
-  eval "$(rbenv init -)"
-
-  if [ -d $HOME/.phpenv/bin ]; then
-    export PATH=$PATH:$HOME/.rbenv/bin:$HOME/.phpenv/bin
-    eval "$(phpenv init -)"
-  fi
-
-fi
-
-# MacOS
-if [ `uname` = 'Darwin' -a -d /usr/local/bin/rbenv/bin ]; then
-  export RBENV_ROOT=$HOME/.rbenv
-  export PATH=$HOME/.rbenv/bin:$PATH
-  eval "$(rbenv init -)"
-
-  # for homebrew
-  export PATH=/usr/local/bin:$PATH
-  export PATH=/usr/local/sbin:$PATH
-fi
-
-# CentOS
-if [ `uname` = 'Linux' -a -d /usr/local/rbenv/bin ]; then
-  export PATH=$PATH:/usr/local/rbenv/bin
-  export RBENV_DIR="/usr/local/rbenv"
-  export RBENV_ROOT="/usr/local/rbenv"
-  eval "$(rbenv init -)"
-fi
 
--- .sh.d/env.sh
+++ .sh.d/env.sh
@@ -6,6 +6,30 @@ export LANG=ja_JP.UTF-8
 #export VISUAL=emacsclient
 #export ALTERNATE_EDITOR=emacs
 
+# PATH ----------------------------------------------------------------
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=/usr/local/share/bin:$PATH
+
+# rbenv & phpenv --------------------------------
+# .rbenv ディレクトリが home にあればパスを通す
+# phpenv は rbenv を内部利用しているため先に rbenv に PATH を通す
+if [ -d $HOME/.rbenv/bin ]; then
+  export RBENV_ROOT=$HOME/.rbenv
+  export PATH=$RBENV_ROOT/bin:$PATH
+  eval "$(rbenv init -)"
+
+  if [ -d $HOME/.phpenv/bin ]; then
+    export PATH=$HOME/.phpenv/bin:$PATH
+    eval "$(phpenv init -)"
+  fi
+fi
+
+# pyvenv ------------------------------------
+if [ -d $HOME/.pyvenv/bin ]; then
+    source $HOME/.pyvenv/bin/activate
+fi
+
 # cygwin --------------------------------------------------------------
 # cygwin の場合にだけ読み込む
 cygwin=false
