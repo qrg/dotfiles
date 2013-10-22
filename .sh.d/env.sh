cf9a4b5bcc0dcaad9e9fd7d1c1edb49b5668d068
qurage <qualle.rhythmus+d@gmail.com>
2013-10-23 04:18:49 +0900

    Fix Path for 'sudo' and 'rbenv'

--- .sh.d/env.sh
+++ .sh.d/env.sh
@@ -7,9 +7,7 @@ export LANG=ja_JP.UTF-8
 #export ALTERNATE_EDITOR=emacs
 
 # PATH ----------------------------------------------------------------
-export PATH=/usr/local/bin:$PATH
-export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/share/bin:$PATH
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/bin:$PATH"
 
 # rbenv & phpenv --------------------------------
 # .rbenv ディレクトリが home にあればパスを通す
@@ -23,6 +21,10 @@ if [ -d $HOME/.rbenv/bin ]; then
     export PATH=$HOME/.phpenv/bin:$PATH
     eval "$(phpenv init -)"
   fi
+elif [-d /usr/local/rbenv ]; then
+  export RBENV_ROOT=/usr/local/rbenv
+  export PATH=$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH
+  eval "$(rbenv init -)"
 fi
 
 # pyvenv ------------------------------------
