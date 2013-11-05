e3dbac5310621c4829c72b0444e849e0192f8fc8
qurage <qualle.rhythmus+d@gmail.com>
2013-11-05 20:12:35 +0900

    Enable syntax highlight colors in less and man commands

--- .sh.d/env.sh
+++ .sh.d/env.sh
@@ -48,4 +48,23 @@ if $cygwin; then
   export TEMP=/tmp
 fi
 
+# less and man command with colors ------------------------------------
+if [ `which source-highlight` ]; then
+  export LESS='-R'
+  export LESSOPEN='| /usr/local/bin/src-hilite-lesspipe.sh %s'
+fi
+
+
+export MANPAGER='less -R'
+man() {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
+}
 
