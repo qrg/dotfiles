b0f66ffb485f386db28a754421e5b603e4f530a7
qurage <qualle.rhythmus+d@gmail.com>
2013-12-28 14:14:21 +0900

    Update node.js v0.10.23 => v0.10.24

--- .sh.d/common.sh
+++ .sh.d/common.sh
@@ -54,7 +54,7 @@ if [ -e ${NvmFile} ]; then
         *-zsh*|*\ zsh*)
             source ${NvmFile}
             [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
-            nvm use v0.10.23
+            nvm use v0.10.24
         ;;
     esac
 fi
