8b27ae5d793056ffde2d3a20e523781081b89300
qrg <qualle.rhythmus+d@gmail.com>
2021-08-08 07:26:00 +0900

    env - Export the variable if ~/.nodenv exists

--- sh/env.sh
+++ sh/env.sh
@@ -117,6 +117,10 @@ case ${OSTYPE} in
     export HOMEBREW_INSTALL_CLEANUP=1
     export GUILE_TLS_CERTIFICATE_DIRECTORY=/usr/local/etc/gnutls/
     export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl@1.1"
-    export NODE_BUILD_DEFINITIONS=$(brew --prefix node-build-update-defs)/share/node-build
+
+    if [ -s ${HOME}/.nodenv ]; then
+      export NODE_BUILD_DEFINITIONS=$(brew --prefix node-build-update-defs)/share/node-build
+    fi
+
     ;;
 esac
