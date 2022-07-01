849c8c31940e69fd8c27ba2252d972e688384676
qrg <qualle.rhythmus+d@gmail.com>
2022-07-01 23:10:35 +0900

    env - XDG Base Directory environment variables
    https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables

--- sh/env.sh
+++ sh/env.sh
@@ -1,6 +1,11 @@
 # Common shell environment variable with bash and zsh
 # =============================================================================
 
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
+
 export LANG=ja_JP.UTF-8
 export LESSCHARSET=utf-8
 
