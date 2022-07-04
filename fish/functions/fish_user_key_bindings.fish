04f08a1038746f9532748c5cc8bebc23a9b90e06
qrg <qualle.rhythmus+d@gmail.com>
2022-07-05 07:52:41 +0900

    fish - Use PatrickF1/fzf.fish
    `fisher install PatrickF1/fzf.fish`

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,13 +1,20 @@
 function fish_user_key_bindings
-  bind \cr peco_select_history
-  bind \cg peco_select_ghq_repository
+  #bind \cr peco_select_history
+  bind \cr _fzf_search_history
+
+  bind \cg\cg peco_select_ghq_repository
+  bind \cgb peco_select_git_branch
+  bind \cgf peco_select_git_file
+  bind \cgs peco_select_git_file_with_status
+  bind \cgl _fzf_search_git_log
+  bind \cgs _fzf_search_git_status
+  bind \cgt peco_select_git_tag
+  bind \cgh peco_select_git_hash
 
   bind \cx\ck peco_kill
-  bind \cx\cb peco_select_git_branch
-  bind \cx\cf peco_select_git_file
-  bind \cx\cs peco_select_git_file_with_status
-  bind \cx\ct peco_select_git_tag
-  bind \cx\ch peco_select_git_hash
+  bind \cxp _fzf_search_processes
+
+  bind \cxf _fzf_search_directory
 
   bind \eq push_line
 
